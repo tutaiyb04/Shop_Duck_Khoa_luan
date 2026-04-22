ef3f0825b6fccec9ffe11873399746f6da12fe2b
Tú Tài <tutaiyb2411@gmail.com>
2026-04-22 17:38:46 +0700

    fix

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -5,7 +5,7 @@ const isLocal = window.location.hostname === "localhost";
 export const API = axios.create({
   baseURL: isLocal
     ? "http://localhost:3000"
-    : "https://shop-duck-api.onrender.com",
+    : "https://tutaiyb04.github.io/Shop_Duck_Khoa_luan",
 });
 
 // tự động đính kèm Token vào Header nếu có
